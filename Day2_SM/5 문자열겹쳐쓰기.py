cb9ace04a99b6c7f7be105ffed71139d8260a4ab
seulmi0827 <dntmfal12@gmail.com>
2024-12-12 01:55:58 +0900

    Update 5 문자열겹쳐쓰기.py

--- Day2_SM/5 문자열겹쳐쓰기.py
+++ Day2_SM/5 문자열겹쳐쓰기.py
@@ -1,5 +1,4 @@
 def solution(my_string, overwrite_string, s):
-    gap = len(my_string)-len(overwrite_string)-s-3
     if len(my_string) > len(overwrite_string) + s :
         return my_string[0:s] + overwrite_string + my_string[s+len(overwrite_string):]
     elif len(my_string) <= len(overwrite_string) + s :
