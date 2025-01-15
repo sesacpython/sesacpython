087c102036b537f2116f3413594a8c17e3a6118f
Yuna-park217 <kelsie_p217@naver.com>
2025-01-15 21:22:11 +0900

    코드처리하기.py

--- week2_yn/코드처리하기.py
+++ week2_yn/코드처리하기.py
@@ -1,18 +1,18 @@
 def solution(code):
     answer = ''
     mode = 0
-    for idx in range(len(code)):
+    for i in range(len(code)):
         if mode == 0:
-            if code[idx] != "1":
-                if idx % 2 == 0:
-                    answer = answer + code[idx]
-            elif code[idx] == "1":
+            if code[i] != "1":
+                if i % 2 == 0:
+                    answer = answer + code[i]
+            elif code[i] == "1":
                 mode = 1
         else:
-            if code[idx] != "1":
-                if idx % 2 == 1:
-                    answer = answer + code[idx]
-            elif code[idx] == "1":
+            if code[i] != "1":
+                if i % 2 == 1:
+                    answer = answer + code[i]
+            elif code[i] == "1":
                 mode = 0
     if answer == "":
         answer = "EMPTY"
